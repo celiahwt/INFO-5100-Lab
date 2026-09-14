2faa00dee592974e9b83a4fb7653e02068ab0098
celiahwt <han.want@northeastern.edu>
2026-09-14 14:34:40 -0400

    Lab 1 hello world

---
+++ lab1 hello world/src/lab1/hello/world/Lab1HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1.hello.world;
+
+/**
+ *
+ * @author celia_hwt
+ */
+public class Lab1HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hello World");
+
+   }
+ 
+    }
+    
